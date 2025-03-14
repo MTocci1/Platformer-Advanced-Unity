c828992a460e388386cfda2c82dcb1f3ccdd05aa
MTocci1 <Mason.Tocci1@marist.edu>
2025-03-13 20:46:12 -0400

    Added player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,66 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1f;
+    [Header("Movement")]
+    public Rigidbody2D rb;
+    public float moveSpeed = 10.0f;
+    private float horizontalMovement;
+
+    [Header("Jump")]
+    public float jumpPower = 5.0f;
+    private int maxJumps = 2;
+    private int jumpsRemaining = 2;
+
+
+    [Header("GroundCheck")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.5f);
+    public LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        transform.Translate(new Vector2(input.x, input.y) * speed * Time.deltaTime);
+        rb.linearVelocity = new Vector2 (horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        isGrounded();
+    }
+
+    public void PlayerMove(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context) 
+    {
+        if (jumpsRemaining > 0)
+        {
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+                jumpsRemaining--;
+            }
+            else if (context.canceled)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+                jumpsRemaining--;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
+    }
+
+    private bool isGrounded()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer)) 
+        {
+            jumpsRemaining = maxJumps;
+            return true;
+        }
+        return false;
     }
 }
